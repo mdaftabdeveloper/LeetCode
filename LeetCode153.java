9014ec8f73c4c0df38b6936409dee684e7c3eca5
mdaftabdeveloper <mdaftabdeveloper@gmail.com>
2026-01-11 19:15:47 +0530

    solved leetcode 153 'find minimum in rotated sorted array'

---
+++ LeetCode153.java
@@ -0,0 +1,31 @@
+public class LeetCode153 {
+    public static void main(String[] args) {
+        int nums[] = { 50, 60, 70, 80, 90, 10, 20, 30, 40 };
+        System.out.println(findMin(nums));
+    }
+
+    public static int findMin(int[] nums) {
+        if (nums[0] < nums[nums.length - 1] || nums.length == 1) {
+            return nums[0];
+        }
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (mid != 0 && nums[mid - 1] > nums[mid]) {
+                return nums[mid];
+            } else if (mid != nums.length - 1 && nums[mid] > nums[mid + 1]) {
+                return nums[mid + 1];
+            } else if (nums[start] <= nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
