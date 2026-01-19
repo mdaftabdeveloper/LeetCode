b242a6cea63a10485d4e2d8878fbb534ee953897
mdaftabdeveloper <mdaftabdeveloper@gmail.com>
2026-01-19 13:29:00 +0530

    solved leetcode 43 'first missing positive'

---
+++ LeetCode43.java
@@ -0,0 +1,26 @@
+public class LeetCode43 {
+    public static void main(String[] args) {
+
+    }
+
+    public int firstMissingPositive(int[] nums) {
+
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            while (nums[i] > 0 && nums[i] <= n && nums[i] != nums[nums[i] - 1]) {
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+        int i;
+        for (i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return i + 1;
+    }
+}
