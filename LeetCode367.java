ecbfcb5fadf266cd2a7297d18c5ffc58cb109626
mdaftabdeveloper <mdaftabdeveloper@gmail.com>
2026-01-21 13:05:09 +0530

    solved leetcode 367 'valid perfect square'

---
+++ LeetCode367.java
@@ -0,0 +1,23 @@
+public class LeetCode367 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPerfectSquare(int num) {
+        int start = 1;
+        int end = num / 2;
+
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+
+            if (mid * mid == num) {
+                return true;
+            } else if (mid * mid < num) {
+                start = (int) (mid + 1);
+            } else {
+                end = (int) (mid - 1);
+            }
+        }
+        return false;
+    }
+}
