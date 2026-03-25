3774905334ee80b44ab377dbdf095e924d359fe6
mdaftabdeveloper <mdaftabdeveloper@gmail.com>
2026-03-25 13:44:49 +0530

    solved leetcode 83 'remove duplicated from sorted list'

---
+++ LeetCode083.java
@@ -0,0 +1,38 @@
+public class LeetCode083 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(head.val);
+        ListNode ans = dummy;
+        ListNode ptr = head.next;
+
+        while (ptr != null) {
+            if (ptr.val != dummy.val) {
+                // ListNode temp = new ListNode(ptr.val);
+                dummy.next = ptr;
+                dummy = dummy.next;
+            }
+            ptr = ptr.next;
+        }
+        dummy.next = null;
+        return ans;
+    }
+}
